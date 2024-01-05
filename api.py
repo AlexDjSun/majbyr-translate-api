dc2ecbeebe1a128868cb629a90b4dc724b02c3eb
Aleksei Ivanov <aleksei@majbyr.com>
2024-01-05 04:22:23 +0200

    Fix typo in translation model path

--- api.py
+++ api.py
@@ -9,7 +9,7 @@ class TranslationRequest(BaseModel):
     src: str
     tgt: str
 
-TRANSLATION_MODEL_PATH = "models/coverted-smugri-v4"
+TRANSLATION_MODEL_PATH = "models/converted-smugri-v4"
 SP_MODEL = "models/spm/flores200_sacrebleu_tokenizer_spm.ext.model"
 LANGUAGES = ['kpv_Cyrl', 'mhr_Cyrl', 'udm_Cyrl', 'eng_Latn', 'est_Latn', 'fin_Latn', 'rus_Cyrl','hun_Latn', 'kca_Cyrl', 'koi_Cyrl', 'krl_Latn', 'liv_Latn', 'lud_Latn', 'lvs_Latn', 'mdf_Cyrl', 'mns_Cyrl', 'mrj_Cyrl', 'myv_Cyrl', 'nob_Latn', 'olo_Latn', 'sma_Latn', 'sme_Latn', 'smj_Latn', 'smn_Latn', 'sms_Latn', 'vep_Latn', 'vro_Latn']
 
